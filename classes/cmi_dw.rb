0c4c1290e50debc9627c71b3b96ef98eb57ac625
Charles Caldwell <bookedge@gmail.com>
2013-03-29 11:00:03 -0500

    Update cmi_dw.rb
    Fixed indention tabs from old IDE I used.

--- classes/cmi_dw.rb
+++ classes/cmi_dw.rb
@@ -3,14 +3,14 @@ require_relative 'oracle_settings'
 
 class CmiDW < Database
 
-	def initialize(*args) 
-		# Build connection string
-    	settings = OracleSettings.new
-		connection_string = "Provider=MSDASQL.1;Password=#{settings.password};"
-		connection_string += "Persist Security Info=True;User ID=#{settings.username};"
-		connection_string += "Extended Properties=\"DSN=#{settings.dsn};"
-		connection_string += "UID=#{settings.username};PWD=#{settings.password};SERVER=#{settings.server};\""
-		super(connection_string)
-	end
+  def initialize(*args) 
+    # Build connection string
+    settings = OracleSettings.new
+    connection_string = "Provider=MSDASQL.1;Password=#{settings.password};"
+    connection_string += "Persist Security Info=True;User ID=#{settings.username};"
+    connection_string += "Extended Properties=\"DSN=#{settings.dsn};"
+    connection_string += "UID=#{settings.username};PWD=#{settings.password};SERVER=#{settings.server};\""
+    super(connection_string)
+  end
 
 end
